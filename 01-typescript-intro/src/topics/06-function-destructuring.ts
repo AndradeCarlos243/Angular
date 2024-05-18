bb31f8bd7157c5cd63869c54c0e774076991b589
AndradeCarlos243 <thehamster_gamer@outlook.com>
2024-05-17 18:05:47 -0600

    Finalización del proyecto en espera de la Deestructuración de funciones

--- 01-typescript-intro/src/topics/06-function-destructuring.ts
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -15,3 +15,36 @@ const tablet: Product =
     description: 'SMARTBITT 10"',
     price: 500
 }
+
+interface TaxCalculationOptions
+{
+    tax: number;
+    products: Product[];
+}
+
+function calculateTax(options: TaxCalculationOptions): number[]
+{
+    let total = 0;
+    options.products.forEach(product =>
+    {
+        total += product.price;
+    });
+
+    return [total, total * options.tax];
+}
+
+const shoppingCart = [telefono, tablet];
+const tax = 0.16;
+
+const result = calculateTax(
+    { 
+        tax: tax,
+        products: shoppingCart,
+    }
+);
+
+console.log(`Total: ${result[0]}, IVA: ${result[1]}`); // Total: 600, IVA: 96
+
+
+
+
